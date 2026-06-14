98d579c2c86f85b0fe730c1854aceeaa7b5eb8d1
Josh <joshuaanigbata@gmail.com>
2026-06-14 16:35:34 +0100

    Imported all the routes to the index.js and routes them all.

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import studentRoutes from "./routes/student.route.js";
+import teacherRoutes from "./routes/teachers.route.js";
+import courseRoutes from "./routes/courses.route.js";
+import attendenceRoutes from "./routes/attendence.route.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 4500;
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use("/api/students", studentRoutes);
+app.use("/api/teachers", teacherRoutes);
+app.use("/api/courses", courseRoutes);
+app.use("/api/attendence", attendenceRoutes);
 
 
 app.listen(PORT, () => {
